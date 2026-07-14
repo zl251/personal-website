6ccdc430919c7a459a484419f92310ed65ca1a1f
zl251 <745325683@qq.com>
2026-07-14 15:22:25 +0800

    移除视频文件，优化部署

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { inspectAttr } from 'kimi-plugin-inspect-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/gerenwangzhang/',
+  base: '/personal-website/',
   plugins: [inspectAttr(), react()],
   resolve: {
     alias: {
